ba057fbae30a0ec2ace7ec5517aeacffd75644f1
Appiopini <appiopini@gmail.com>
2021-04-02 21:22:11 -0300

    making the css

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'pets/index'
-  get 'pets/show'
-  get 'pets/new'
-  get 'pets/create'
-  get 'pets/edit'
-  get 'pets/update'
-  get 'pets/destroy'
+  resources :pets
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
